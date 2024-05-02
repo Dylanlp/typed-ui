572ded66e9dae476e1830488aea135a9fd6f9236
Dylanlp <68389971+Dylanlp@users.noreply.github.com>
2024-05-02 10:17:50 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import favicon from "/favicon.ico";
+import favicon from "./favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
